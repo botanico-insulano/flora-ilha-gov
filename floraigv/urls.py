7177be4eacc1e25488e059e60a879d51f5e6c1fb
Aron-PC <aron.b.96@gmail.com>
2025-06-28 15:07:25 -0300

    adjust url consts

--- floraigv/urls.py
+++ floraigv/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import hello_view, hello_filename
 
 urlpatterns = [
-	path(f'{hello_filename}/', hello_view, name='hello')
+	path(f'{hello_filename}/', hello_view, name=hello_filename)
 ]
--- plant_catalog_backend/urls.py
+++ plant_catalog_backend/urls.py
@@ -4,11 +4,17 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 from floraigv.apps import FloraigvConfig
+from floraigv.views import hello_filename
 
-print(f'{FloraigvConfig.name}.urls')
+flora_app_urls = f'{FloraigvConfig.name}.urls'
+
+print(flora_app_urls)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', RedirectView.as_view(url='hello/')),
-    path('', include(f'{FloraigvConfig.name}.urls')),
+    path('', include(flora_app_urls)),
+    path('', RedirectView.as_view(url=f'{hello_filename}/')),
 ]
+
+# [apps.py] app name set -> [views.py] function for rendering template by [app_name]/[filename].html
+# -> [urls.py] path to find function -> [self] include app paths and redirect
